efa0d9b7492449950274c76054e8bb509ce4354d
Sharanya Udayakumar <sharanyaudayakumar7@gmail.com>
2026-04-09 00:40:20 +0530

    fixed render port issue

--- app.py
+++ app.py
@@ -79,5 +79,8 @@ def home():
 
     return render_template("index.html", caption=caption, image_path=image_path)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
